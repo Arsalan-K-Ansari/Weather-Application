31e63ac82b91469336b7c7179b15494170d0a807
Arsalan <arsalank.ansari@gmail.com>
2023-07-24 18:20:34 -0400

    aaaa

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // dependencies
-var searchFormEl = document.querySelector("#search");
-var searchInputEl = document.querySelector("#search-input");
-var searchHistoryEl = document.querySelector("#history");
-var currentWeatherEl = document.querySelector("#current");
-var forecastEl = document.querySelector("#5day");
-var previousSearches = localStorage.getItem("WeatherSearches");
-var searchList = previousSearches ? JSON.parse(previousSearches) : [];
+var searchForm = document.querySelector("#search");
+var searchInput = document.querySelector("#search-input");
+var searchBtn = document.querySelector("#search-bttn");
+var searchHistory = document.querySelector("#history");
+var currentWeather = document.querySelector("#current");
+var forecast = document.querySelector("#5day");
+var cityNameEl = document.querySelector("#city-name");
+var tempEl = document.querySelector("#temperature");
+var windEl = document.querySelector("#wind speed");
+var humidityEl = document.querySelector("#humidity");
+var cityWeather;
 
 function makeRequest() {
-  var openWeatherAPIKey = "67500ba9ac78c4f9498327daec0bfbc6";
-  var city = searchCity.value;
+  var openWeatherAPIKey = "4f9c37dbda4edac1d11258b52be2b443";
+  var city = searchInput.value;
   console.log(city);
   var weatherQuery =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -30,3 +34,48 @@ function makeRequest() {
       humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
     });
 }
+function displayStuff() {
+  var lastCity = JSON.parse(localStorage.getItem("localWeather"));
+  var lastSearchBttn = document.createElement("button");
+  lastSearchBttn.textContent = lastCity.name;
+  lastSearchBttn.classList.add("btn");
+  searchHistory.append(lastSearchBttn);
+  lastSearchBttn.addEventListener("click", function () {
+    currentWeather.textContent = savedCity.name;
+    var openWeatherAPIKey = "4f9c37dbda4edac1d11258b52be2b443";
+    var city = savedCity.name;
+    console.log(city);
+    var weatherQuery =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      openWeatherAPIKey;
+
+    fetch(weatherQuery)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        windEl.textContent = "Wind Speed: " + data.wind.speed + "mph";
+        tempEl.textContent =
+          "Temperature: " +
+          Math.floor(((data.main.temp - 273.15) * 9) / 5 + 32) +
+          "F";
+        humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
+      });
+  });
+}
+searchBtn.on("click", function (event) {
+  event.preventDefault;
+  if (searchInput.value !== " ") {
+    makeRequest();
+    cityWeather = {
+      name: searchInput.value,
+    };
+    cityNameEl.textContent = searchInput.value;
+    localStorage.setItem("localWeather", JSON.stringify(cityWeather));
+    searchInput.value = " ";
+
+    displayStuff();
+  }
+});
